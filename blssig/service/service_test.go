6a51566a308dd84b1bfd89e360975b007b3f413f
11721387 <e11721t387@student.tuwien.ac.at>
2019-02-23 12:00:58 +0100

    test shell

---
+++ blssig/service/service_test.go
@@ -0,0 +1,43 @@
+package service
+
+import (
+	"testing"
+
+	proofofloc "github.com/dedis/student_19_proof-of-loc/blssig"
+	"github.com/stretchr/testify/require"
+	"go.dedis.ch/kyber/v3/pairing"
+	"go.dedis.ch/onet/v3"
+	"go.dedis.ch/onet/v3/log"
+)
+
+const protoName = "testService"
+
+var testSuite = pairing.NewSuiteBn256()
+
+func TestMain(m *testing.M) {
+	log.MainTest(m)
+}
+
+func TestService_Sign(t *testing.T) {
+	local := onet.NewTCPTest(testSuite)
+	// generate 5 hosts, they don't connect, they process messages, and they
+	// don't register the tree or entitylist
+	hosts, roster, _ := local.GenTree(5, true)
+	defer local.CloseAll()
+
+	message := []byte("Message")
+	services := local.GetServices(hosts, testID)
+
+	for _, s := range services {
+		log.Lvl2("Sending request to", s)
+		resp, err := s.(*SimpleBLSCoSiService).Sign(
+			&proofofloc.Signed{
+				Roster: roster,
+				ToSign: message,
+			},
+		)
+		require.Nil(t, err)
+		require.NotNil(t, resp)
+		//require.Equal(t, resp.Children, len(roster.List))
+	}
+}
